c6e75051e6b3a90c7b5dda94c6a52fad389f0b67
hyy <1714776581@qq.com>
2022-05-19 13:03:20 +0800

    第一次修改

---
+++ readme.md
@@ -0,0 +1,17 @@
+### `说明：本程序使用selenium自动化浏览器操作，以此实现看自动看智慧树网课的需要，因此无需担心被检测`
+### `使用所需：python, lxml, selenium, chromedriver, chrome浏览器`
+###`使用说明：` `首先需要安装Chrome或Firefox浏览器，推荐chrome`
+###`然后安装python及lxml,selenium库`
+###`在浏览器信息中查看你的Chrome版本,下载对应的Chromedriver版本，然后将下载的文件与此程序放至同一目录下即可`
+####_http://chromedriver.storage.googleapis.com/index.html_
+###`修改代码中username, password 为你的手机号与密码`
+## **`最重要的一步，输入你需要自动播放的课程的url`**
+### 举个例子：这是课程视频第一节的url
+![img.png](img.png)
+### 这是最后一节课的url
+![img_1.png](img_1.png)
+### 观察可以发现仅改变了filed后的数字，因此只需将代码中此处的数字改为{i},并将start_num改为视频开始的数字，将end_num改为视频结束的数字即可，其余不变即可
+![img_2.png](img_2.png)
+这里添加了一个不打开浏览器的选择，只要将此行代码注释去掉便可以不打开浏览器了。
+实现这样一个东西其实挺简单的，这里仅仅是提供了selenium的一个小应用，其实只要简单了解一下这个包，便能很熟练的使用一些基本功能了。
+若遇到错误，请检查网络条件，或直接修改代码。
---
+++ wangke.py
@@ -0,0 +1,87 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.common.exceptions import TimeoutException
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.wait import WebDriverWait
+from lxml import etree
+import time
+
+
+class Auto_play:
+    # http://chromedriver.storage.googleapis.com/index.html
+    _path_of_chromedriver = 'E:\py\diy\demo\wangke\chromedriver'
+    _browser = None
+    _url_homepage = 'https://passport.zhihuishu.com/login'
+    _header_data = {
+        'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+        'accept-encoding': 'gzip, deflate',
+        'accept-language': 'zh-CN,zh;q=0.9',
+        'upgrade-insecure-requests': '1',
+        'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.123 Safari/537.36',
+    }
+    chrome_options = webdriver.ChromeOptions()
+    # chrome_options.add_argument('--headless')  # 配置无头浏览器，就是不打开浏览器运行界面
+    chrome_options.add_argument("--mute-audio")
+
+    def __init__(self, username, password):
+        self.username = username
+        self.password = password
+
+        self._browser = webdriver.Chrome(options=self.chrome_options, executable_path='./chromedriver.exe')
+        self._browser.get(self._url_homepage)
+        self.wait = WebDriverWait(self._browser, 10)
+
+        self.login()
+
+    def login(self):
+        time.sleep(3)
+        self.wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, '#lUsername'))).send_keys(self.username)
+        self.wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, '#lPassword'))).send_keys(self.password)
+        self.wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, '#f_sign_up > div.switch-wrap.switch-wrap-signin'
+                                                                     '.active > span'))).click()
+        time.sleep(3)
+
+        print('Login Success')
+
+    def quit_browser(self):
+        self._browser.quit()
+
+    def get_page_by_url(self, urls):
+        for url in urls:
+            self._browser.get(url)
+            try:
+                button = self.wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, '#playButton')))
+                print(url)
+                button.click()
+            except TimeoutException:
+                print('失败')
+
+            time.sleep(20)
+            text = etree.HTML(self._browser.page_source)
+            duration = text.xpath('//*[@id="vjs_mediaPlayer"]/div[5]/div[4]/div/text()')
+            try:
+                print(duration[0], int(duration[0].split(':')[0]) * 60 + int(duration[0].split(':')[1]))
+                dur = int(duration[0].split(':')[0]) * 60 + int(duration[0].split(':')[1])
+                if dur == 0:
+                    print('获取时间失败')
+                    time.sleep(900)
+                else:
+                    time.sleep(dur + 20)
+            except IndexError:
+                print('time load fail')
+                time.sleep(900)
+
+
+if __name__ == '__main__':
+    username = ''
+    password = ''
+    start_num = 0
+    end_num = 0
+
+    auto = Auto_play(username, password)
+    url = []
+
+    for i in range(start_num, end_num+1):
+        url.append(f'{i}')
+    auto.get_page_by_url(url)
+    auto.quit_browser()
